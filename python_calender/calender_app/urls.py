fd453dff6a165b475fb5b7db723d7f695e7aa9f9
NimraSadaqat <nimra.sadaqat@gmail.com>
2021-09-01 21:54:58 -0700

    Final Calender App

--- python_calender/calender_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'calendar_app'
-
-urlpatterns = [
-    # path('', views.index, name='main-view'),
-    path('', views.CalendarView.as_view(), name='calendars'),
-]
--- python_calender_mongodb/calender_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CalenderAppConfig(AppConfig):
-    name = 'calender_app'
